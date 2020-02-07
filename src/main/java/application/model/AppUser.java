b9e83f73fdc1e025c15ee70d1bf7771116520a63
Izabela Łach <ilach@opi.org.pl>
2020-02-07 17:55:40 +0100

    'tabela_sie_nie_tworzy'

--- src/main/java/application/model/AppUser.java
+++ src/main/java/application/model/AppUser.java
@@ -1,5 +1,22 @@
 package application.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "APPUSER")
+@Getter
+@Setter
+@NoArgsConstructor
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String pass;
 }
